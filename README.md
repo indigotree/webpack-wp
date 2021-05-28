240e53a4f71506ca793e693d6f1f816024c23f7f
Christopher Geary <chris.geary@indigotree.co.uk>
2021-05-28 14:22:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Webpack Config for WordPress
+
+A shared Webpack config for WordPress projects.
+
+## Usage
+
+This config will assume the entry point of your project is `src/index.js` and will output the result in `dist/`.
+
+## License
+
+GPL-3.0+
---
+++ index.js
@@ -0,0 +1,70 @@
+const path = require("path");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+const DependencyExtractionWebpackPlugin = require("@wordpress/dependency-extraction-webpack-plugin");
+
+const isDev = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: isDev ? "development" : "production",
+  stats: "minimal",
+  entry: {
+    index: path.resolve(process.cwd(), "src", "index.js"),
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@wordpress/babel-preset-default"]
+          }
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require("autoprefixer")(),
+                  require("postcss-flexbugs-fixes"),
+                ],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|avif|woff2?|ttf|eot|otf)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "[contenthash][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "chunk-[id].css",
+    }),
+    new DependencyExtractionWebpackPlugin(),
+    new WebpackManifestPlugin(),
+  ],
+};
